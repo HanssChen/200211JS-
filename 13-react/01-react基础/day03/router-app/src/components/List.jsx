7fb91fe24386b559d200ef95f0d5898c6046d227
yujingbo <1935952410@qq.com>
2020-06-18 16:23:35 +0800

    xx

---
+++ 13-react/01-react基础/day03/router-app/src/components/List.jsx
@@ -0,0 +1,11 @@
+import React,{ Component } from "react"; 
+
+export default class List extends Component{
+    render(){
+        return(
+            <div>
+                <h3>列表页组件</h3>
+            </div>
+        )
+    }
+}
